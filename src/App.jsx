d4add3c1782c7192bbbc576d14751fa4ceb653f0
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-21 18:16:28 +0100

    import-react-components-css-in App.jsx-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 //Creare un semplice form con un campo input per il titolo di un articolo del blog. Al submit del form, mostrare la lista degli articoli inseriti, con la possibilità di cancellare ciascun articolo utilizzando un'icona.
 
 
+import { useState } from 'react'
+import AppHeader from './components/AppHeader.jsx'
+import AppMain from './components/AppMain.jsx'
+import AppFooter from './components/AppFooter.jsx'
+import AppCss from './App.css'
+
 function App() {
 
     return (
