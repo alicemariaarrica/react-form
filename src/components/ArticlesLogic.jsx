bd00b1f93b01013efaa8d0ada2235b5841c58dc8
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-21 19:37:41 +0100

    function-addArticle-in-ArticlesLogic.jsx

--- src/components/ArticlesLogic.jsx
+++ src/components/ArticlesLogic.jsx
@@ -6,3 +6,16 @@ export default function ArticlesLogic() {
     const [articles, setArticles] = useState(articles)
     const [newArticle, setNewArticle] = useState('')
 }
+
+function addArticle(e) {
+
+    e.preventDefault()
+    console.log(e); //dopo aver controllato se ho bug devo toglierla
+    setArticle([
+    ..article,
+        newArticle
+
+    ])
+    setNewArticle('')
+
+}
