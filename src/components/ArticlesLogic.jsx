4b60bd4a876ff1b48eba375b9e28929f4530d734
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-21 19:30:20 +0100

    function-for-state-articles&newArticles-commit

--- src/components/ArticlesLogic.jsx
+++ src/components/ArticlesLogic.jsx
@@ -1,3 +1,8 @@
 
 import { useState } from 'react';
 import articles from './data/articles.js'
+
+export default function ArticlesLogic() {
+    const [articles, setArticles] = useState(articles)
+    const [newArticle, setNewArticle] = useState('')
+}
