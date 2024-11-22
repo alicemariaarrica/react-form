e87321708da1e1378ffbb5fefd0f464d16dac195
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-22 17:16:21 +0100

    h1-commit

--- src/components/ArticlesLogic.jsx
+++ src/components/ArticlesLogic.jsx
@@ -8,7 +8,7 @@ export default function ArticlesLogic() {
         e.preventDefault()
         console.log(e); //dopo aver controllato se ho bug devo toglierla
         setArticle([
-            ...article,
+            ...articles,
             newArticle
 
         ])
@@ -22,13 +22,19 @@ export default function ArticlesLogic() {
         const indexToDelete = Number(objectEvent.target.getAttribute('data-index'));
         console.log(article, indexToDelete);
 
-        const newArticles = article.filter((article, index) => index != indexToDelete);
+        const newArticles = article.filter((articles, index) => index != indexToDelete);
         console.log(newArticles);
-        setArticle(newArticles)
+        setArticles(newArticles)
 
 
     }
 
+    return (
+        <div>
+            <h1>Gestione Articoli Blog</h1>
+        </div>)
 }
+
+//Creare un semplice form con un campo input per il titolo di un articolo del blog. Al submit del form, mostrare la lista degli articoli inseriti, con la possibilità di cancellare ciascun articolo utilizzando un'icona.
 
 
